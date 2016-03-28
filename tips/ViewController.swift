00712f98c601b14699e19d4df8b8f5ff920107b2
cchannap <cchannap@cisco.com>
2016-03-27 21:25:58 -0700

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by cchannap on 3/27/16.
+//  Copyright (c) 2016 cchannap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
